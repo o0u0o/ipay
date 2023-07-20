4c33cee83baa5997110ac05a6f6c56ef83169080
o0u0o <1553122309@qq.com>
2023-07-20 09:39:58 +0800

    [文档]更新文档注释

--- src/main/java/com/o0u0o/ipay/common/enumeration/type/SignType.java
+++ src/main/java/com/o0u0o/ipay/common/enumeration/type/SignType.java
@@ -6,7 +6,9 @@ import java.util.Map;
 /**
  * 签名方式.
  * <p>
- * 即时到账(老接口)支持MD5和RSA的签名方式, App支付和Wap支付支持RSA和RSA2的签名方式. DSA签名方式暂时不支持.
+ *     即时到账(老接口)支持MD5和RSA的签名方式, App支付和Wap支付支持RSA和RSA2的签名方式. DSA签名方式暂时不支持.
+ * </p>
+ *
  */
 public enum SignType {
     MD5, RSA, RSA2;
